b5ab9e73a30bbea90d680335e0530749c51b71f8
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2018-02-16 12:39:12 -0600

    Initial Code

---
+++ PS4.R
@@ -0,0 +1,7 @@
+myFunction <- function (doorthing, doorthing2, x) {
+  doorthing1 <- doorthing2 <- sample(1:3,1)
+  if (doorthing1==doorthing2) { x<-TRUE } else { x == FALSE }
+  x
+}
+
+myFunction(sample(1:3, 1), sample(1:3,1)))
